fd09b4bd4d5f0cbff5ebc999dc699082ad2a861b
star2000 <i@star2000.work>
2022-08-16 19:49:39 +0800

    切换github镜像

--- lib/core.ps1
+++ lib/core.ps1
@@ -1,7 +1,7 @@
 function ConvertTo-MirrorUrl ($Url) {
     $map = @{
-        '//github.com/'                = '//hub.verge.tk/';
-        '//raw.githubusercontent.com/' = '//raw.verge.tk/'
+        '//github.com/'                = '//ghproxy.com/https://github.com/';
+        '//raw.githubusercontent.com/' = '//ghproxy.com/https://raw.githubusercontent.com/'
     }
     if ($map.Keys | Where-Object { $Url -match $_ }) {
         if ($null -eq $env:SCOOP_INGFW) {
