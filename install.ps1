28d32173ba466a0bd24709fa9aa26888ed8a4d8d
star2000 <i@star2000.work>
2022-08-16 19:49:39 +0800

    切换github镜像

--- install.ps1
+++ install.ps1
@@ -561,8 +561,8 @@ function Write-DebugInfo {
 
 function ConvertTo-MirrorUrl ($Url) {
     $map = @{
-        '//github.com/'                = '//hub.verge.tk/';
-        '//raw.githubusercontent.com/' = '//raw.verge.tk/'
+        '//github.com/'                = '//ghproxy.com/https://github.com/';
+        '//raw.githubusercontent.com/' = '//ghproxy.com/https://raw.githubusercontent.com/'
     }
     if ($map.Keys | Where-Object { $Url -match $_ }) {
         if ($null -eq $env:SCOOP_INGFW) {
