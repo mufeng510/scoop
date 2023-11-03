e047a9cc6bb6b90f024363ea5d14b749bd32b91c
star2000 <i@star2000.work>
2023-11-03 14:42:07 +0800

    切换github镜像

--- lib/core.ps1
+++ lib/core.ps1
@@ -1,7 +1,7 @@
 function ConvertTo-MirrorUrl ($Url) {
     $map = @{
-        '//github.com/'                = '//ghproxy.com/https://github.com/';
-        '//raw.githubusercontent.com/' = '//ghproxy.com/https://raw.githubusercontent.com/'
+        '//github.com/'                = '//gh-proxy.com/https://github.com/';
+        '//raw.githubusercontent.com/' = '//gh-proxy.com/https://raw.githubusercontent.com/'
     }
     if ($map.Keys | Where-Object { $Url -match $_ }) {
         if ($null -eq $env:SCOOP_INGFW) {
